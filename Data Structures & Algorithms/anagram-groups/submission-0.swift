b9749096ad06bf94eb28bbb0adb29d8d5c738841
Mahmoud Amer <mahmoud.ahmed.am01@gmail.com>
2026-09-17 13:06:11 +0200

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.swift
+++ Data Structures & Algorithms/anagram-groups/submission-0.swift
@@ -1,12 +1,18 @@
 class Solution {
     func groupAnagrams(_ strs: [String]) -> [[String]] {
-        var hashMap = [String: [String]]()
+        let aAsciiValue = Character("a").asciiValue!
+
+        var groupedAnagrams = [[Int]: [String]]()
 
         for str in strs {
-            let sorted = String(str.sorted())
-            hashMap[sorted, default: []].append(str)
+            var arr = [Int](repeating: 0, count: 26)
+            for char in str {
+                let index = Int(char.asciiValue! - aAsciiValue)
+                arr[index] += 1
+            }
+            groupedAnagrams[arr, default: []].append(str)
         }
 
-        return Array(hashMap.values)
+        return Array(groupedAnagrams.values)
     }
 }
